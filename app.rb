3957b5838e0e9d9b30e58a61ac0bc2e11e09714a
Frida Kiriakos <fkiriakos@cigital.com>
2017-07-07 10:04:04 -0700

    add api call and display resulting deals

--- app.rb
+++ app.rb
@@ -1,6 +1,8 @@
 require 'sinatra/base'
 require 'sinatra/twitter-bootstrap'
 require 'pry'
+require 'rest-client'
+require 'json'
 
 # this class initializes our application for displaying deals
 class TopDealsApp < Sinatra::Base
@@ -8,6 +10,29 @@ class TopDealsApp < Sinatra::Base
 
   get '/' do
     haml :index
+  end
+
+  post '/search' do
+    response = RestClient.get('https://offersvc.expedia.com/offers/v2/getOffers',
+                              params: search_params(request.params))
+    parsed_response = JSON.parse(response, symbolize_names: true)
+    hotels = parsed_response[:offers][:Hotel]
+    @offers = filter_and_sort_results(hotels) unless hotels.nil?
+    haml :search
+  end
+
+  helpers do
+    def search_params(params)
+      {
+        scenario: 'deal-finder', page: 1, uid: 1, productType: 'Hotel',
+        destinationCity: params['destinationCity']
+      }
+    end
+
+    def filter_and_sort_results(results)
+      results.select  { |offer| offer[:hotelPricingInfo][:percentSavings] > 0 }
+             .sort_by { |offer| offer[:hotelPricingInfo][:totalPriceValue] }
+    end
   end
 end
 
